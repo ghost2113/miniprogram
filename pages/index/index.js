a8bb631a1c6934a075668ffb27da5dcd1edc680f
tianjinying93@163.com <tianjinying93@163.com>
2018-07-18 22:24:21 +0800

    0718

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,42 +3,56 @@ const order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
     systemInfo:"",
-    kindData:'',
-    toView: 'red',
-    scrollTop: 100
-  },
-  upper: function (e) {
-    console.log(e)
-  },
-  lower: function (e) {
-    console.log(e)
-  },
-  scroll: function (e) {
-    console.log(e)
-  },
-  tap: function (e) {
-    for (var i = 0; i < order.length; ++i) {
-      if (order[i] === this.data.toView) {
-        this.setData({
-          toView: order[i + 1]
-        })
-        break
-      }
-    }
-  },
-  tapMove: function (e) {
-    this.setData({
-      scrollTop: this.data.scrollTop + 10
-    })
+    kindData:''
   },
   onLoad: function () {
     const that = this;
-    wx.getSystemInfo({
+    wx.request({
+      url: 'https://zhishun520.com/zaotoutiao-api-home-1.0.0//news/channels/',
+      data: {
+        userId: 929
+      },
+      method: 'GET',
+      // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'content-type': 'application/json'
+      },//默认为 'application/json'，
       success: function (res) {
+        // success
         console.log(res);
         that.setData({
-          systemInfo: res
+          kindData: res.data.data
         })
+      },
+      fail: function () {
+        // fail
+      }
+    })
+  },
+  onReachBottom: function () {
+    var that = this;
+    // 显示加载图标  
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+    // 页数+1  
+    //page = page + 1;
+    wx.request({
+      url: 'https://zhishun520.com/zaotoutiao-api-home-1.0.0/user/record/packet',
+      method: "GET",
+      // 请求头部  
+      header: {
+        'content-type': 'application/text'
+      },
+      success: function (res) {
+        // 回调函数  
+        console.log("上拉", res);
+        // 设置数据  
+        // that.setData({
+        //   moment: that.data.moment
+        // })
+        // 隐藏加载框  
+        wx.hideLoading();
       }
     })
   },
